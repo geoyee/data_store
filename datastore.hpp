e503d45f49fda805b98c1c34d557dda0129e3385
geoyee <geoyee@yeah.net>
2024-03-06 13:08:58 +0800

    feat: Init

---
+++ datastore.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifndef DATASTORE_HPP
+#define DATASTORE_HPP
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class DataStore
+{
+};
+
+#endif // DATASTORE_HPP
